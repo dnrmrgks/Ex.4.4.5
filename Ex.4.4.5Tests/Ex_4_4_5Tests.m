843bcffb842c6115197a8c72b6230958f72d3cd7
park sung chan <dnrmrgks@naver.com>
2014-01-07 20:10:04 +0900

    Initial Commit

---
+++ Ex.4.4.5Tests/Ex_4_4_5Tests.m
@@ -0,0 +1,34 @@
+//
+//  Ex_4_4_5Tests.m
+//  Ex.4.4.5Tests
+//
+//  Created by SDT-1 on 2014. 1. 7..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex_4_4_5Tests : XCTestCase
+
+@end
+
+@implementation Ex_4_4_5Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
