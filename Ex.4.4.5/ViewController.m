3070547944acae9d806309a75220466a9e54b6e8
park sung chan <dnrmrgks@naver.com>
2014-01-07 21:55:35 +0900

    4.4.5
    웹

--- Ex.4.4.5/ViewController.m
+++ Ex.4.4.5/ViewController.m
@@ -9,11 +9,48 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
 
+-(BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    if(UIWebViewNavigationTypeLinkClicked == navigationType){
+        
+        NSLog(@"링크클릭은 금지!");
+        return NO;
+    }
+    return YES;
+    
+}
+- (IBAction)go:(id)sender {
+    
+    if([self.textField.text length]>3){
+        
+        NSURL *url = [NSURL URLWithString:self.textField.text];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+        [self.textField resignFirstResponder];
+    }
+}
+
+-(void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    
+    NSString *msg = [error localizedDescription];
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"에러" message:msg delegate:Nil cancelButtonTitle:@"확인" otherButtonTitles: nil];
+                          [alert show];
+}
+                          
+                          -(BOOL)textFieldShouldReturn:(UITextField *)textField{
+                              [self go:nil];
+                              return YES;
+                          }
+
+                          
+                          
+                          
 - (void)viewDidLoad
 {
     [super viewDidLoad];
